cf128d0ad257a2f5a710f18efb55e0c6550898eb
peach lasagna <kir.kud@inbox.ru>
2021-06-08 15:22:25 +0700

    suka

--- bot.py
+++ bot.py
@@ -3,8 +3,6 @@ import httpx
 from aiogram.types import InputFile
 from aiogram import Bot, Dispatcher, executor, types
 from os import getenv
-from urllib.parse import urlparse
-
 
 
 bot = Bot(token=getenv('TOKEN'))
@@ -18,13 +16,15 @@ async def send_welcome(message: types.Message):
 
 @dp.message_handler(commands="meow")
 async def meow(message: types.Message):
-    url = "https://aws.random.cat/meow"
-    async with httpx.AsyncClient() as client:
-        res = await client.get(url)
-    url_meow = res.json()['file']
-    file = InputFile.from_url(url_meow, filename=url_meow[url_meow.index('/i/')+3:])
-    await message.answer_photo(file)
-
+    try:
+        url = "https://aws.random.cat/meow"
+        async with httpx.AsyncClient() as client:
+            res = await client.get(url)
+        url_meow = res.json()['file']
+        file = InputFile.from_url(url_meow, filename=url_meow[url_meow.index('/i/')+3:])
+        await message.answer_photo(file)
+    except:
+        pass
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
